00ddaa16ce989c3b6ce4f13f2d04199c3d1181bd
Pompiliu Girlonta <pompiliu74@gmail.com>
2021-06-02 20:23:43 -0700

    added in new shapes, and editted the main file to allow swapping them out

--- covid-rage-room.js
+++ covid-rage-room.js
@@ -1,7 +1,7 @@
 import {defs, tiny} from './examples/common.js';
 
 const {
-    Vector, Vector3, vec, vec3, vec4, color, hex_color, Matrix, Mat4, Light, Shape, Material, Scene,
+    Vector, Vector3, vec, vec3, vec4, color, hex_color, Matrix, Mat4, Light, Shape, Material, Scene, Shader, Texture, Component 
 } = tiny;
 
 class Cube extends Shape {
@@ -70,6 +70,98 @@ class Cube_Single_Strip extends Shape {
     }
 }
 
+export
+const Shape_From_File = defs.Shape_From_File =
+class Shape_From_File extends Shape
+{                                   // **Shape_From_File** is a versatile standalone Shape that imports
+                                    // all its arrays' data from an .obj 3D model file.
+  constructor( filename )
+    { super( "position", "normal", "texture_coord" );
+                                    // Begin downloading the mesh. Once that completes, return
+                                    // control to our parse_into_mesh function.
+      this.load_file( filename );
+    }
+  load_file( filename )
+      {                             // Request the external file and wait for it to load.
+        return fetch( filename )
+          .then( response =>
+            { if ( response.ok )  return Promise.resolve( response.text() )
+              else                return Promise.reject ( response.status )
+            })
+          .then( obj_file_contents => this.parse_into_mesh( obj_file_contents ) )
+          .catch( error => { throw "OBJ file loader:  OBJ file either not found or is of unsupported format." } )
+      }
+  parse_into_mesh( data )
+    {                           // Adapted from the "webgl-obj-loader.js" library found online:
+      var verts = [], vertNormals = [], textures = [], unpacked = {};
+
+      unpacked.verts = [];        unpacked.norms = [];    unpacked.textures = [];
+      unpacked.hashindices = {};  unpacked.indices = [];  unpacked.index = 0;
+
+      var lines = data.split('\n');
+
+      var VERTEX_RE = /^v\s/;    var NORMAL_RE = /^vn\s/;    var TEXTURE_RE = /^vt\s/;
+      var FACE_RE = /^f\s/;      var WHITESPACE_RE = /\s+/;
+
+      for (var i = 0; i < lines.length; i++) {
+        var line = lines[i].trim();
+        var elements = line.split(WHITESPACE_RE);
+        elements.shift();
+
+        if      (VERTEX_RE.test(line))   verts.push.apply(verts, elements);
+        else if (NORMAL_RE.test(line))   vertNormals.push.apply(vertNormals, elements);
+        else if (TEXTURE_RE.test(line))  textures.push.apply(textures, elements);
+        else if (FACE_RE.test(line)) {
+          var quad = false;
+          for (var j = 0, eleLen = elements.length; j < eleLen; j++)
+          {
+              if(j === 3 && !quad) {  j = 2;  quad = true;  }
+              if(elements[j] in unpacked.hashindices)
+                  unpacked.indices.push(unpacked.hashindices[elements[j]]);
+              else
+              {
+                  var vertex = elements[ j ].split( '/' );
+
+                  unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 0]);
+                  unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 1]);
+                  unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 2]);
+
+                  if (textures.length)
+                    {   unpacked.textures.push(+textures[( (vertex[1] - 1)||vertex[0]) * 2 + 0]);
+                        unpacked.textures.push(+textures[( (vertex[1] - 1)||vertex[0]) * 2 + 1]);  }
+
+                  unpacked.norms.push(+vertNormals[( (vertex[2] - 1)||vertex[0]) * 3 + 0]);
+                  unpacked.norms.push(+vertNormals[( (vertex[2] - 1)||vertex[0]) * 3 + 1]);
+                  unpacked.norms.push(+vertNormals[( (vertex[2] - 1)||vertex[0]) * 3 + 2]);
+
+                  unpacked.hashindices[elements[j]] = unpacked.index;
+                  unpacked.indices.push(unpacked.index);
+                  unpacked.index += 1;
+              }
+              if(j === 3 && quad)   unpacked.indices.push( unpacked.hashindices[elements[0]]);
+          }
+        }
+      }
+      {
+      const { verts, norms, textures } = unpacked;
+        for( var j = 0; j < verts.length/3; j++ )
+        {
+          this.arrays.position     .push( vec3( verts[ 3*j ], verts[ 3*j + 1 ], verts[ 3*j + 2 ] ) );
+          this.arrays.normal       .push( vec3( norms[ 3*j ], norms[ 3*j + 1 ], norms[ 3*j + 2 ] ) );
+          this.arrays.texture_coord.push( vec( textures[ 2*j ], textures[ 2*j + 1 ] ) );
+        }
+        this.indices = unpacked.indices;
+      }
+      this.normalize_positions( false );
+      this.ready = true;
+    }
+  draw( caller, uniforms, model_transform, material )
+    {               // draw(): Same as always for shapes, but cancel all
+                    // attempts to draw the shape before it loads:
+      if( this.ready )
+        super.draw( caller, uniforms, model_transform, material );
+    }
+}
 
 export class Rage_Room extends Scene {
     constructor() {
@@ -80,6 +172,13 @@ export class Rage_Room extends Scene {
         this.shapes = {
             'cube': new Cube(),
             'outline': new Cube_Outline(),
+            "teapot": new Shape_From_File("assets/teapot.obj"),
+            "amogus": new Shape_From_File( "assets/amogus.obj"),
+            "amogus1": new Shape_From_File( "assets/amogusPiece1.obj"),
+            "amogus2": new Shape_From_File( "assets/amogusPiece2.obj"),
+            "cube01": new Shape_From_File( "assets/cube01.obj"),
+
+
         };
 
         // *** Materials
@@ -136,7 +235,7 @@ export class Rage_Room extends Scene {
         let max_bounces = init_height;
 
         // use -20 for gravity, decrease max height over time
-        const acc = 20;
+        const acc = 32;
         let max_height = Math.max(init_height - time_elapsed, 0);
         let init_velocity = Math.sqrt(2 * acc * max_height);
         let period = 2 / acc * init_velocity;
@@ -170,7 +269,7 @@ export class Rage_Room extends Scene {
 
 
 
-        const blue = hex_color("#1a9ffa");
+        const blue = hex_color("#fa751a");
 
         let model_transform = Mat4.identity();
         let initial_height = 10;
@@ -180,7 +279,9 @@ export class Rage_Room extends Scene {
         // base rests at 0
         model_transform = model_transform.times(Mat4.translation(0,height + 2,0))
             .times(Mat4.scale(2,2,2));
-        this.shapes.cube.draw(context, program_state, model_transform, this.materials.plastic.override({color:blue}));
+        this.shapes.cube01.draw(context, program_state, model_transform, this.materials.plastic.override({color:blue}));
+        //this.shapes.amogus2.draw(context, program_state, model_transform, this.materials.plastic.override({color:blue}));
+
 
         let room_transform = Mat4.identity();
         room_transform = room_transform.times(Mat4.translation(0,20,0))
